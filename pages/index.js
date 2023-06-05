cee784df85a7a20b7589c2d496ea4fdf79e86556
Mohd Ejaz Siddiqui <mohdejazsiddiqui@.com>
2023-06-05 13:31:56 +0400

    updated readme, added a sample env file

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import {useEffect, useState} from "react";
+import {useState} from "react";
 
 export default function Home() {
 	const [prompt, setPrompt] = useState('')
